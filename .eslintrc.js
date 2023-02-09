effea9f0db9ff8288984d3e315566f0ab6f36209
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-02-08 21:05:02 -0300

    chore: changed some config and rule for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
-module.exports = {
+export default {
     "env": {
         "browser": true,
-        "es2021": true,
-        "node": true
+        "es2021": true
+    },
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
     },
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended"
     ],
     "overrides": [
     ],
@@ -18,8 +23,14 @@ module.exports = {
     },
     "plugins": [
         "react",
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "react-hooks"
     ],
     "rules": {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "react/jsx-uses-react": "off",
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
     }
 }
